cbcb5ffb8822273e211f703232560973bc279a47
maartimolina <percellomar@gmail.com>
2023-05-12 23:11:41 -0300

    1205

--- prjSp1MiPrimerApp/FrmAlacena.cs
+++ prjSp1MiPrimerApp/FrmAlacena.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -52,6 +53,68 @@ namespace prjSp1MiPrimerApp
 
         }
 
+        private void lstNombre_Click(object sender, EventArgs e)
+        {
+            lstNombre.Items.Clear();
+            if (btnComida.Checked)
+            {
+                lstNombre.Items.Add("Dulce de Leche");
+                lstNombre.Items.Add("Mermelada");
+                lstNombre.Items.Add("Soda");
+                lstNombre.Items.Add("Queso Rallado");
+                lstNombre.Items.Add("Chocolate en Polvo");
+                lstNombre.Items.Add("Azucar");
+                lstNombre.Items.Add("Fideos");
+                lstNombre.Items.Add("Café");
+                lstNombre.Items.Add("Té");
+                lstNombre.Items.Add("Arroz");
+                lstNombre.Items.Add("Milanesas");
+                lstNombre.Items.Add("Gaseosa");
+                lstNombre.Items.Add("Leche");
+                lstNombre.Items.Add("Aceite de Girasol");
+                lstNombre.Items.Add("Aceite de Oliva");
+                lstNombre.Items.Add("Vinagre");
+                lstNombre.Items.Add("Avena");
+                lstNombre.Items.Add("Cereales");
+            }
+            else
+            {
+                if (btnLimpieza.Checked)
+                {
+                    lstNombre.Items.Add("Lavandina");
+                    lstNombre.Items.Add("Perfumina");
+                    lstNombre.Items.Add("Jabon");
+                    lstNombre.Items.Add("Detergente");
+                    lstNombre.Items.Add("Pasta de Dientes");
+                    lstNombre.Items.Add("Shampoo");
+                    lstNombre.Items.Add("Acondicionador");
+
+                }
+                else
+                {
+                    if (btnMedicamento.Checked)
+                    {
+                        lstNombre.Items.Add("Ibuprofeno");
+                        lstNombre.Items.Add("Novalgina");
+                        lstNombre.Items.Add("Paracetamol");
+                        lstNombre.Items.Add("Vendas");
+                        lstNombre.Items.Add("Curitas");
+
+
+
+                    }
+
+
+
+
+                }
+
+
+            }    
+            
+
+        }
+        
         public frmAlacena()
         {
             InitializeComponent();
@@ -76,7 +139,7 @@ namespace prjSp1MiPrimerApp
             lstNombre.Items.Add("Pasta de Dientes");
             lstNombre.Items.Add("Shampoo");
             lstNombre.Items.Add("Acondicionador");
-            lstNombre.Items.Add("Dulce de Lecha");
+            lstNombre.Items.Add("Dulce de Leche");
             lstNombre.Items.Add("Mermelada");
             lstNombre.Items.Add("Soda");
             lstNombre.Items.Add("Queso Rallado");
@@ -87,6 +150,11 @@ namespace prjSp1MiPrimerApp
             lstNombre.Items.Add("Vinagre");
             lstNombre.Items.Add("Avena");
             lstNombre.Items.Add("Cereales");
+            lstNombre.Items.Add("Ibuprofeno");
+            lstNombre.Items.Add("Novalgina");
+            lstNombre.Items.Add("Paracetamol");
+            lstNombre.Items.Add("Vendas");
+            lstNombre.Items.Add("Curitas");
 
 
 
